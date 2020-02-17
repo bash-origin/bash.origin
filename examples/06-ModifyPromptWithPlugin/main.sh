378787502a15d1f4e78da782b3adffc3d78d494b
Christoph Dorn <christoph@christophdorn.com>
2020-02-16 19:52:02 -0800

    timer improvements, call plugin with pinf.it interface

--- examples/06-ModifyPromptWithPlugin/main.sh
+++ examples/06-ModifyPromptWithPlugin/main.sh
@@ -4,18 +4,25 @@ eval BO_SELF_BASH_SOURCE="$BO_READ_SELF_BASH_SOURCE"
 BO_deriveSelfDir ___TMP___ "$BO_SELF_BASH_SOURCE"
 local __BO_DIR__="$___TMP___"
 
+echo "---"
 
 echo "TEST_MATCH_IGNORE>>>"
-BO_callDownloadedPlugin "bash.origin.prompt@0.1.1" setPrompt "workspace" "$__BO_DIR__"
+BO_callDownloadedPlugin "bash.origin.prompt@0.2.0-pre.1#prompt/v0" setPrompt "workspace" "$__BO_DIR__"
 echo "<<<TEST_MATCH_IGNORE"
+
+echo "$PS1"
+
+echo "---"
 
 # or
 
 echo "TEST_MATCH_IGNORE>>>"
-BO_ensureInSystemCache "DOWNLOADED_PATH" "github.com/bash-origin/bash.origin.prompt" "0.1.1" "https://github.com/bash-origin/bash.origin.prompt/archive/v0.1.1.zip"
-BO_callPlugin "$DOWNLOADED_PATH/bash.origin.prompt" setPrompt "workspace" "$__BO_DIR__"
+BO_ensureInSystemCache "DOWNLOADED_PATH" "github.com/bash-origin/bash.origin.prompt" "0.2.0-pre.1" "https://github.com/bash-origin/bash.origin.prompt/archive/v0.2.0-pre.1.zip"
+BO_callPlugin "$DOWNLOADED_PATH/bash.origin.prompt#prompt/v0" setPrompt "workspace" "$__BO_DIR__"
 echo "<<<TEST_MATCH_IGNORE"
 
-# TODO: Assertions
+echo "$PS1"
+
+echo "---"
 
 echo "OK"
